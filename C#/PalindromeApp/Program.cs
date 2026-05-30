9a3feb3d29d56b0e7fb3fdb3ca662938997d0931
Melquisedeque <melquisedequebass@gmail.com>
2026-05-30 00:22:38 -0300

    coding two pointer (palindrome) code in c#

---
+++ C#/PalindromeApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+  public static void Main()
+  {
+    var sentences = new List<string>() {
+      "A man, a plan, a canal: Panama",
+      "race a car",
+      "Do geese see God?"
+    };
+
+    sentences.ForEach(s => Console.WriteLine($"{s} {IsPalindrome(s)}"));
+  }
+
+  private static bool IsPalindrome(string phrase)
+  {
+    if (string.IsNullOrEmpty(phrase))
+      return false;
+
+    int left = 0, right = phrase.Length - 1;
+
+    while (left < right)
+    {
+      while (left < right && !char.IsLetterOrDigit(phrase[left])) left++;
+      while (left < right && !char.IsLetterOrDigit(phrase[right])) right--;
+
+      if (char.ToLowerInvariant(phrase[left]) != char.ToLowerInvariant(phrase[right]))
+        return false;
+
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+}
---
+++ C#/TwoSumApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class TwoSumModel
+{
+  public int[] Nums { get; }
+  public int Target { get; }
+
+  public TwoSumModel(int[] _nums, int _target)
+  {
+    Nums = _nums;
+    Target = _target;
+  }
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    var tests = new List<TwoSumModel>()
+    {
+      new TwoSumModel([2, 8, 11, 15, 7], 9),
+      new TwoSumModel([11, 3, 5, 2, 15], 5),
+      new TwoSumModel([3, 2, 4, 8, 9, 20, 13], 16),
+      new TwoSumModel([2, 5, 0, 5], 10)
+    };
+
+    tests.ForEach(t =>
+    {
+      Console.WriteLine($"[{string.Join(", ", TwoSum(t.Nums, t.Target))}]");
+    });
+  }
+
+  private static int[] TwoSum(int[] nums, int target)
+  {
+    var numToIndex = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+      int complement = target - nums[i];
+
+      if (numToIndex.TryGetValue(complement, out int x))
+        return [x, i];
+
+      numToIndex.Add(nums[i], i);
+    }
+
+    return [];
+  }
+}
